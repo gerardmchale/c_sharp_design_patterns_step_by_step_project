e1c27b7e07e95b8d40bb43298b22afe9b13f0668
Gerard McHale <gerard.mchale@electroroute.com>
2015-07-22 00:11:05 +0100

    Create Add button click function

--- WindowsCustomerUI/Form1.Designer.cs
+++ WindowsCustomerUI/Form1.Designer.cs
@@ -171,6 +171,7 @@
             this.btnAdd.TabIndex = 7;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnUpdate
             // 
--- WindowsCustomerUI/Form1.cs
+++ WindowsCustomerUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomerLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace WindowsCustomerUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            CustomerBase custbase = null;
+            if (cmbCustomerType.SelectedIndex == 0)
+            {
+                custbase = new Lead();
+            }
+            else
+            {
+                custbase = new Customer();
+            }
+            custbase.CustomerName = txtCustomerName.Text;
+            custbase.Address = txtAddress.Text;
+            custbase.PhoneNumber = txtPhoneNumber.Text;
+            custbase.BillDate = Convert.ToDateTime(txtBillingDate.Text);
+            custbase.BillAmount = Convert.ToDecimal(txtBillingAmount.Text);
         }
     }
 }
