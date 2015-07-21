a5850c10dbc2bdac2df8a57c4569b238f6134415
Gerard McHale <gerard.mchale@electroroute.com>
2015-07-21 21:58:59 +0100

    Create initial objects for project

---
+++ CustomerLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerLibrary
+{
+    public class Lead
+    {
+        public string LeadName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Address { get; set; }
+    }
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Address { get; set; }
+    }
+}
