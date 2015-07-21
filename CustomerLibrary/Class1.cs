ec8e9a5af9f62bece799653740199920986c1b61
Gerard McHale <gerard.mchale@electroroute.com>
2015-07-21 22:05:10 +0100

    Override validate method in Lead class

--- CustomerLibrary/Class1.cs
+++ CustomerLibrary/Class1.cs
@@ -40,6 +40,16 @@ namespace CustomerLibrary
 
     public class Lead : Customer
     {
-
+        public override void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+        }
     }
 }
