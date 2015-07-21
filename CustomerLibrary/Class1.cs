04051fefc4a222695f002446c8fe6a1f5247a899
Gerard McHale <gerard.mchale@electroroute.com>
2015-07-21 22:03:40 +0100

    Add validate method to Customer class

--- CustomerLibrary/Class1.cs
+++ CustomerLibrary/Class1.cs
@@ -13,6 +13,29 @@ namespace CustomerLibrary
         public decimal BillAmount { get; set; }
         public DateTime BillDate { get; set; }
         public string Address { get; set; }
+
+        /// <summary>
+        /// Validate the customer values
+        /// </summary>
+        public virtual void Validate()
+        {
+            if (CustomerName.Length == 0)
+            {
+                throw new Exception("Customer Name is required");
+            }
+            if (PhoneNumber.Length == 0)
+            {
+                throw new Exception("Phone number is required");
+            }
+            if (BillAmount > 0)
+            {
+                throw new Exception("Bill is required");
+            }
+            if (BillDate >= DateTime.Now)
+            {
+                throw new Exception("Bill date  is not proper");
+            }
+        }
     }
 
     public class Lead : Customer
